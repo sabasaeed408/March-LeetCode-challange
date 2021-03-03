38af53a3c8e0b3498e98e90db83c5cfa3dadf4c8
Saba Saeed <62014225+sabasaeed408@users.noreply.github.com>
2021-03-03 13:18:15 +0500

    Create D2 LeetCode--> SetMismatch.py

---
+++ D2 LeetCode--> SetMismatch.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        l, s = len(nums), sum(set(nums))
+        l = l * ( 1 + l ) // 2
+        return [sum(nums) - s, l - s]
+        
